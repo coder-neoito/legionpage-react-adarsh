325acd0214510f5d3fd3586f752b16c93f9d633c
Adarsh S <adarsh.s@neoito.com>
2023-05-05 15:47:25 +0530

    intital commit-setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Grid, Stack } from "@mui/material";
+
+const userData = {
+  avatar:
+    "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  userName: "Pogative",
+  name: "Connor",
+  state: "Califonria",
+  country: "United States",
+  level: 10,
+  xp: 500,
+  about: "Anything with strategy building",
+};
+
+export default function App() {
+  return (
+    <Stack gap={3}>
+      <Grid container spacing={5}>
+        <Grid item xs={8}></Grid>
+      </Grid>
+    </Stack>
+  );
+}
