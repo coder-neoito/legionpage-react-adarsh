6e94988fac47e957a49eb731a7bd6fde01c449a5
Adarsh S <adarsh.s@neoito.com>
2023-05-05 17:12:15 +0530

    add home page an routing

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import User from "./components/User";
 import { Grid, Stack } from "@mui/material";
 import { activities, userData } from "./data";
 import Online from "./components/Online";
+import { Routes, Route } from "react-router-dom";
+import UserDetails from "./routes/UserDetails";
+import Home from "./routes/Home";
 
 export default function App() {
   return (
-    <Stack gap={3} p={3}>
-      <User userData={userData} />
-      <Grid container spacing={5}>
-        <Grid item xs={8}>
-          <Activities
-            duration="82.5 hours past two weeks"
-            activities={activities}
-          />
-        </Grid>
-        <Grid item xs={4}>
-          <Online data={{ badges: 6, games: 60, reviews: 5 }} />
-        </Grid>
-      </Grid>
-    </Stack>
+    <Routes>
+      <Route Component={Home} path="/" />
+      <Route Component={UserDetails} path="/:userName" />
+    </Routes>
   );
 }
